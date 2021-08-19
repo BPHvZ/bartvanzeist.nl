ecb1671a26586406fbbc5e995b68abd7e7a06340
Bart van Zeist <bartvanzeist2000@gmail.com>
2021-08-19 16:53:06 +0200

    improve service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  "mode": "production",
   "globDirectory": "_site/",
   "globPatterns": [
     "**/*.{css,ttf,woff,woff2,png,jpeg,ico,jpg,js,html,json}"
   ],
-  "swDest": "_site/sw.js"
+  "swDest": "_site/sw.js",
+  "skipWaiting": true
 };
